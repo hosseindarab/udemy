ccc955d6c992ea14b83e2cf72d7b371f1c53d5b9
hossein <hossein.darab@intrum.com>
2023-10-11 16:56:08 +0200

    - CRUD operations

--- rest-apis/app/Models/Task.php
+++ rest-apis/app/Models/Task.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Task extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'is_done'
+    ];
+
+    protected $casts = [
+        'is_done' => 'boolean'
+    ];
+    protected $hidden = [
+        'updated_at'
+    ];
 }
