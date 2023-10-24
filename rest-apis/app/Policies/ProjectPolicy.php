66a184e2402ca89967936dfeaf9c54250178f1e9
darabho <hossein.darab@intrum.com>
2023-10-24 14:20:33 +0200

    - ProjectPolicy bug fixed

--- rest-apis/app/Policies/ProjectPolicy.php
+++ rest-apis/app/Policies/ProjectPolicy.php
@@ -37,7 +37,7 @@ class ProjectPolicy
      */
     public function update(User $user, Project $project): bool
     {
-        return user->id === $project->creator_id;
+        return $user->id === $project->creator_id;
     }
 
     /**
