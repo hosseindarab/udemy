e5c9e3e903de06b4f5a74f5de3d299ebc1d0ab89
darabho <hossein.darab@intrum.com>
2023-10-27 12:39:18 +0200

    - Testing->JSON structure - Testing->filterable fields

--- rest-apis/tests/Feature/Controllers/TaskController/TaskControllerIndexTest.php
+++ rest-apis/tests/Feature/Controllers/TaskController/TaskControllerIndexTest.php
@@ -2,6 +2,8 @@
 
 namespace Tests\Feature\TaskController;
 
+use App\Models\Task;
+use PHPUnit\Framework\Attributes\DataProvider;
 use Tests\TestCase;
 use App\Models\User;
 use Laravel\Sanctum\Sanctum;
@@ -10,11 +12,50 @@ class TaskControllerIndexTest extends TestCase
 {
     public function test_authenticated_users_can_fetch_the_task_list(): void
     {
-        Sanctum::actingAs(User::factory()->create());
+        $user = User::factory()->create();
+        Sanctum::actingAs($user);
+
+        Task::factory()->for($user, 'creator')->create();
 
         $route = route("tasks.index");
         $response = $this->getJson($route);
-        $response->assertOk();
+
+        $response->assertOk()
+            ->assertJsonCount(1, 'data')
+            ->assertJsonStructure([
+                'data' => [
+                    '*' => [
+                        'id',
+                        'title',
+                        'is_done',
+                        'status',
+                        'created_at',
+                        'updated_at',
+                    ]
+                ]
+            ]);
+    }
+
+    /**
+     * @dataProvider filterFields
+     */
+    public function test_filterable_fields($field, $value, $expectedCode): void
+    {
+        $user = User::factory()->create();
+        Sanctum::actingAs($user);
+
+        $route = route('tasks.index', ["filter[{$field}]" => $value]);
+        $response = $this->getJson($route);
+        $response->assertStatus($expectedCode);
+    }
+
+    public static function filterFields(): array
+    {
+        return [
+            ['id', 1, 400],
+            ['title', 'foo', 400],
+            ['is_done', 1, 200],
+        ];
     }
 
     public function test_unauthenticated_users_can_not_fetch_the_task_lists(): void
